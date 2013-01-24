2152798d6250bb529937e5b3b77a6a453c0c4bc1
John McLear <john@mclear.co.uk>
2013-01-24 22:07:31 +0000

    make it asnyc

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var eejs = require('ep_etherpad-lite/node/eejs')
-  , padManager = require('ep_etherpad-lite/node/db/PadManager')
-  , log4js = require('log4js')
-  , logger = log4js.getLogger("plugin:ep_list_pads");
+  , padManager = require('ep_etherpad-lite/node/db/PadManager');
 
 exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_list_pads/templates/letters.ejs");
@@ -10,30 +8,41 @@ exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
 
 exports.registerRoute = function (hook_name, args, cb) {
   args.app.get('/list/:letter(*)', function(req, res) {
+
     var letter = req.params.letter;
 
-    var pads=padManager.listAllPads().padIDs
-      , data={
-        progress : 1
-        , message: "Search done."
-        , query: letter
-        , total: pads.length
-      }
-      , maxResult=0
-      , result=[]
-    ;
-
-    pads.forEach(function(padID){
-      if(padID[0] == letter || padID[0] == letter.toUpperCase()){
-        result.push(padID);
-      }
+    search(letter, function(pads){
+      var render_args = {
+        errors: [],
+        pads: pads,
+        letter: letter
+      };
+      res.send( eejs.require("ep_list_pads/templates/pads.html", render_args) );
     });
 
-    var render_args = {
-      errors: [],
-      pads: result,
-      letter: letter
-    };
-    res.send( eejs.require("ep_list_pads/templates/pads.html", render_args) );
   });
 };
+
+var search = function(letter, callback){
+  var pads=padManager.listAllPads().padIDs
+    , data={
+      progress : 1
+      , message: "Search done."
+      , letter: letter
+      , total: pads.length
+    }
+    , maxResult=0
+    , result=[]
+  ;
+  var data = [];
+
+  pads.forEach(function(padID){
+    if(padID[0] == letter || padID[0] == letter.toUpperCase()){
+      data.push(padID);
+    }
+  });
+
+  callback(data);
+}
+
+
