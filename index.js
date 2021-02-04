487758a316c13fac2bb4cf581bea885517e12f04
John McLear <john@mclear.co.uk>
2021-02-04 13:35:14 +0000

    lint

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
+'use strict';
+
 const eejs = require('ep_etherpad-lite/node/eejs/');
 const padManager = require('ep_etherpad-lite/node/db/PadManager');
-const ERR = require('ep_etherpad-lite/node_modules/async-stacktrace');
-const async = require('ep_etherpad-lite/node_modules/async');
-const express = require('ep_etherpad-lite/node_modules/express');
 
-exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
+exports.eejsBlock_indexWrapper = (hookName, args, cb) => {
   args.content += eejs.require('ep_list_pads/templates/letters.ejs');
   return cb();
 };
 
-exports.registerRoute = function (hook_name, args, cb) {
+exports.registerRoute = (hookName, args, cb) => {
   args.app.get('/list/:letter(*)', async (req, res) => {
-    const data = [];
     const letter = req.params.letter.toLowerCase();
     let pads = await padManager.listAllPads();
     pads = pads.padIDs;
 
-    const render_args = {};
+    const renderArgs = {};
 
     if (letter === 'hash') {
       // all others.
-      render_args.pads = pads.filter((pad) => pad.toLowerCase().match(/^[0-9]/i));
-      render_args.description = 'a number';
+      renderArgs.pads = pads.filter((pad) => pad.toLowerCase().match(/^[0-9]/i));
+      renderArgs.description = 'a number';
     } else {
-      render_args.pads = pads.filter((pad) => pad.toLowerCase().startsWith(letter.toLowerCase()));
-      render_args.description = `the letter ${letter}`;
+      renderArgs.pads = pads.filter((pad) => pad.toLowerCase().startsWith(letter.toLowerCase()));
+      renderArgs.description = `the letter ${letter}`;
     }
 
-    res.send(eejs.require('ep_list_pads/templates/pads.html', render_args));
+    res.send(eejs.require('ep_list_pads/templates/pads.html', renderArgs));
   });
+  cb();
 };
